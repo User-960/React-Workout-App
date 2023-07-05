37de824e33dd78db81ed744780d6844b1a62c283
Ivan <ivanvolodin960@gmail.com>
2023-07-06 00:10:10 +0300

    feat: create hook useOutside and add logics in component Hamburger

--- src/components/layout/hamburger/Hamburger.tsx
+++ src/components/layout/hamburger/Hamburger.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react'
 import styles from './Hamburger.module.scss'
+import { CgMenuRight, CgClose } from 'react-icons/cg'
+import { useOutside } from '@/components/hooks/useOutside'
+import Menu from './Menu'
 
 const Hamburger: FC = () => {
+  const { ref, isShow, setIsShow } = useOutside(false)
   return (
     <div className={styles.wrapper}>
-      Hamburger
+      <button onClick={() => { setIsShow(!false) }}>
+        {isShow ? <CgClose /> : <CgMenuRight />}
+      </button>
+      {isShow && (<div className={styles.menu} ref={ref}><Menu /></div>)}
     </div>
   )
 }
---
+++ src/components/layout/hamburger/Menu.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Menu: FC = () => {
+  return <menu>Menu</menu>
+}
+
+export default Menu
