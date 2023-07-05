716b035420aaa49a51d45a6ddc9b580eaeb031ad
Ivan <ivanvolodin960@gmail.com>
2023-07-05 23:18:45 +0300

    feat: create elements for Header

---
+++ src/components/layout/hamburger/Hamburger.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Hamburger: FC = () => {
+  return <div>Hamburger</div>
+}
+
+export default Hamburger
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react'
 import styles from './Header.module.scss'
+import { useAuth } from '@/components/hooks/useAuth'
+import { FiArrowLeft } from 'react-icons/fi'
+import Hamburger from '../hamburger/Hamburger'
 
 const Header: FC = () => {
+  const { isAuth } = useAuth()
   return (
     <header className={styles.header}>
-      <h2>Header</h2>
+      <button onClick={() => { }}>
+        <FiArrowLeft />
+      </button>
+
+      <Hamburger />
     </header>
   )
 }
