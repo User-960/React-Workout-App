abb068c89a6ebde31169504c977eec7e563b4e2d
Ivan <ivanvolodin960@gmail.com>
2023-07-06 14:54:00 +0300

    feat: add types for auth on page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { Inter } from 'next/font/google'
 import { NextPage } from 'next'
+
 import Home from '@/components/screens/home/Home'
 
-const inter = Inter({ subsets: ['latin'] })
-
 const HomePage: NextPage = () => {
-  return <Home />
+	return <Home />
 }
 
 export default HomePage
