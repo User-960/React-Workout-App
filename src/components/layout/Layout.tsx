556137f4c0b1c2219420c8496f1013259c012537
Ivan <ivanvolodin960@gmail.com>
2023-07-06 13:30:56 +0300

    feat: add styles for component Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,37 @@
-import { FC, PropsWithChildren } from 'react'
+import cn from 'clsx'
+import { FC, ReactNode } from 'react'
 
 import Meta from '../seo/Meta'
 import { IMeta } from '../seo/meta.interface'
 
+import styles from './Layout.module.scss'
 import Header from './header/Header'
 
-const Layout: FC<PropsWithChildren<IMeta>> = ({
-	title,
-	description,
-	children
+interface ILayoutProps {
+	bgImage?: any
+	heading?: string
+	backLink?: string
+	children: ReactNode
+
+	meta: IMeta
+}
+
+const Layout: FC<ILayoutProps> = ({
+	bgImage = '',
+	heading = '',
+	backLink = '/',
+	children,
+	meta
 }) => {
 	return (
 		<>
-			<Meta title={title} description={description}>
-				<Header />
-				<main>{children}</main>
+			<Meta title={meta.title} description={meta.description}>
+				<section
+					className={cn(styles.wrapper, { [styles.otherPage]: !!heading })}
+				>
+					<Header />
+					<main>{children}</main>
+				</section>
 			</Meta>
 		</>
 	)
--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,13 +1,21 @@
-import Layout from '@/components/layout/Layout'
 import { FC } from 'react'
+
+import Layout from '@/components/layout/Layout'
+import { IMeta } from '@/components/seo/meta.interface'
+
 import styles from './Home.module.scss'
 
 const Home: FC = () => {
-  return (
-    <Layout title='Home' description='Sport is a life'>
-      <h1 className={styles.title}>Home</h1>
-    </Layout>
-  )
+	const meta: IMeta = {
+		title: 'Home',
+		description: 'Sport is a life'
+	}
+
+	return (
+		<Layout meta={meta}>
+			<h1 className={styles.title}>Home</h1>
+		</Layout>
+	)
 }
 
 export default Home
