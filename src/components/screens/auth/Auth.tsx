c5279061ef6da4b441411c0a74037fa3d8fac194
Ivan <ivanvolodin960@gmail.com>
2023-07-08 12:52:32 +0300

    feat: add Buttons and styles

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 import Button from '@/components/ui/button/Button'
@@ -8,12 +8,15 @@ import Loader from '@/components/ui/loader/Loader'
 import Layout from '@/components/layout/Layout'
 import { IMeta } from '@/components/seo/meta.interface'
 
+import styles from './Auth.module.scss'
 import { IAuthFields } from '@/interfaces/form.interface'
 
-const isLoading: boolean = true
+const isLoading: boolean = false
 const isLoadingAuth: boolean = false
 
 const Auth: FC = () => {
+	const [type, setType] = useState<string>('auth')
+
 	const {
 		register,
 		handleSubmit,
@@ -28,13 +31,15 @@ const Auth: FC = () => {
 	}
 
 	const onSubmit: SubmitHandler<IAuthFields> = data => {
+		/*TODO: type*/
 		console.log(data)
 	}
 
 	return (
-		<Layout meta={meta} heading='Sign in' bgImage='/images/auth-bg.png'>
+		<>
+			<Layout meta={meta} heading='Sign in' bgImage='/images/auth-bg.png' />
 			<div className='wrapper-inner-page'>
-				{true && <Loader />}
+				{(isLoading || isLoadingAuth) && <Loader />}
 
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<Field
@@ -54,15 +59,21 @@ const Auth: FC = () => {
 						error={errors?.password?.message}
 						name='password'
 						register={register}
-						required={'*Password is required!'}
+						required={
+							'*Password is required! It must include: 0-9, a-Z, A-Z, !@#$%^&*'
+						}
 						pattern={
 							/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{4,}/g
 						}
 					/>
-					<Button>Let`s go</Button>
+
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Sing in</Button>
+						<Button clickHandler={() => setType('reg')}>Sing up</Button>
+					</div>
 				</form>
 			</div>
-		</Layout>
+		</>
 	)
 }
 
