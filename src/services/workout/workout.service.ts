b26e892f090b6f7710918c63740617ba692d3433
Ivan <ivanvolodin960@gmail.com>
2023-07-12 14:16:33 +0300

    feat: create workout service

---
+++ src/services/workout/workout.service.ts
@@ -0,0 +1,33 @@
+import { $axios } from '@/api/api'
+import { IExercise } from '@/interfaces/exercise.interface'
+
+const WORKOUTS: string = '/workouts'
+
+class WorkoutService {
+	async getAll() {
+		return $axios.get<any>(WORKOUTS)
+	}
+
+	async getById(id: number) {
+		return $axios.get<any>(`${WORKOUTS}/${id}`)
+	}
+
+	async create(name: string, exerciseIds: number[]) {
+		return $axios.post<any>(WORKOUTS, {
+			name,
+			exerciseIds
+		})
+	}
+
+	async update(id: number, exerciseIds: number[]) {
+		return $axios.put<any>(`${WORKOUTS}/${id}`, {
+			exerciseIds
+		})
+	}
+
+	async delete(id: number) {
+		return $axios.delete<any>(`${WORKOUTS}/${id}`)
+	}
+}
+
+export default new WorkoutService()
