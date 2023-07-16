e35d10647832f2400455b057eaec249d335887d2
Ivan <ivanvolodin960@gmail.com>
2023-07-17 00:52:18 +0300

    fix: fix bug with component NotFound

--- src/components/screens/not-found/NotFound.tsx
+++ src/components/screens/not-found/NotFound.tsx
@@ -1,4 +1,7 @@
-import { FC } from 'react'
+import { useRouter } from 'next/router'
+import { FC, useEffect } from 'react'
+
+import { useAuth } from '@/components/hooks/useAuth'
 
 import Layout from '@/components/layout/Layout'
 import { IMeta } from '@/components/seo/meta.interface'
@@ -8,6 +11,15 @@ const NotFound: FC = () => {
 		title: 'Not Found',
 		description: 'Page is not found'
 	}
+
+	const { isAuth } = useAuth()
+	const { push } = useRouter()
+
+	useEffect(() => {
+		if (!isAuth) {
+			push('/auth')
+		}
+	}, [])
 
 	return (
 		<>
